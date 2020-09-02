68f5074be403f1e28cf826ea931e1722aa783d28
Kamil Wojtczyk <k.wojtczyk@gmail.com>
2020-09-03 00:27:08 +0200

    fix: improve parsing algorithm

--- asciicanvas/lib/asciicanvas.ex
+++ asciicanvas/lib/asciicanvas.ex
@@ -10,26 +10,38 @@ defmodule Asciicanvas do
 
       iex> Asciicanvas.parse_input("Rectangle at `[3,2]` with width: `5`, height: `3`, outline character: `@`, fill character: `X`")
       %{
-        "fill" => "X",
-        "height" => "3",
-        "outline" => "@",
-        "type" => "Rectangle",
-        "width" => "5",
-        "x" => "3",
-        "y" => "2"
+        fill: "X",
+        height: "3",
+        outline: "@",
+        type: :rectangle,
+        width: "5",
+        x: "3",
+        y: "2"
       }
   """
   def parse_input(input) do
-    regex =
-      ~r/(?<type>\w+)\[(?<x>.+),(?<y>.+)\].+width:(?<width>.+).+height:(?<height>.+).+outline:(?<outline>.+).+fill:(?<fill>.+)/
+    type =
+      Regex.run(~r/^\w+/, input)
+      |> hd
+      |> String.downcase()
+      |> String.to_atom()
 
-    String.replace(input, " ", "")
-    |> String.replace("`", "")
-    |> String.replace("at", "")
-    |> String.replace("character", "")
-    |> (&Regex.named_captures(
-          regex,
-          &1
-        )).()
+    [x, y] =
+      String.replace(input, ~r{(.+\[|\].+| )}, "")
+      |> String.split(",")
+
+    extract_property = fn input, property ->
+      clean_regex = ~r/(`| |:|character)/
+
+      String.replace(input, clean_regex, "")
+      |> String.replace(~r/(.+#{property}|,.+)/, "")
+    end
+
+    width = extract_property.(input, "width")
+    height = extract_property.(input, "height")
+    outline = extract_property.(input, "outline")
+    fill = extract_property.(input, "fill")
+
+    %{type: type, x: x, y: y, width: width, height: height, outline: outline, fill: fill}
   end
 end
