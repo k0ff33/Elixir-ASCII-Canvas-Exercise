3b3c46a3b34b1fa3ec0f295446974fe6c31b0d9d
Kamil Wojtczyk <k.wojtczyk@gmail.com>
2020-09-03 00:27:23 +0200

    chore: add tests provided in the task

--- asciicanvas/test/asciicanvas_test.exs
+++ asciicanvas/test/asciicanvas_test.exs
@@ -1,4 +1,139 @@
 defmodule AsciicanvasTest do
   use ExUnit.Case
   doctest Asciicanvas
+
+  test "parse input with test fixture 1 (1)" do
+    assert Asciicanvas.parse_input(
+             "Rectangle at [3,2] with width: 5, height: 3, outline character: `@`, fill character: `X`"
+           ) ==
+             %{
+               fill: "X",
+               height: "3",
+               outline: "@",
+               type: :rectangle,
+               width: "5",
+               x: "3",
+               y: "2"
+             }
+  end
+
+  test "parse input with test fixture 1 (2)" do
+    assert Asciicanvas.parse_input(
+             "Rectangle at [10, 3] with width: 14, height: 6, outline character: `X`, fill character: `O`"
+           ) ==
+             %{
+               fill: "O",
+               height: "6",
+               outline: "X",
+               type: :rectangle,
+               width: "14",
+               x: "10",
+               y: "3"
+             }
+  end
+
+  test "parse input with test fixture 2 (1)" do
+    assert Asciicanvas.parse_input(
+             "Rectangle at `[14, 0]` with width `7`, height `6`, outline character: none, fill: `.`"
+           ) ==
+             %{
+               fill: ".",
+               height: "6",
+               outline: "none",
+               type: :rectangle,
+               width: "7",
+               x: "14",
+               y: "0"
+             }
+  end
+
+  test "parse input with test fixture 2 (2)" do
+    assert Asciicanvas.parse_input(
+             "Rectangle at `[0, 3]` with width `8`, height `4`, outline character: `O`, fill: `none`"
+           ) ==
+             %{
+               fill: "none",
+               height: "4",
+               outline: "O",
+               type: :rectangle,
+               width: "8",
+               x: "0",
+               y: "3"
+             }
+  end
+
+  test "parse input with test fixture 2 (3)" do
+    assert Asciicanvas.parse_input(
+             "Rectangle at `[5, 5]` with width `5`, height `3`, outline character: `X`, fill: `X`"
+           ) ==
+             %{
+               fill: "X",
+               height: "3",
+               outline: "X",
+               type: :rectangle,
+               width: "5",
+               x: "5",
+               y: "5"
+             }
+  end
+
+  test "parse input with test fixture 3 (1)" do
+    assert Asciicanvas.parse_input(
+             "Rectangle at `[14, 0]` with width `7`, height `6`, outline character: none, fill: `.`"
+           ) ==
+             %{
+               fill: ".",
+               height: "6",
+               outline: "none",
+               type: :rectangle,
+               width: "7",
+               x: "14",
+               y: "0"
+             }
+  end
+
+  test "parse input with test fixture 3 (2)" do
+    assert Asciicanvas.parse_input(
+             "Rectangle at `[0, 3]` with width `8`, height `4`, outline character: `O`, fill: `none`"
+           ) ==
+             %{
+               fill: "none",
+               height: "4",
+               outline: "O",
+               type: :rectangle,
+               width: "8",
+               x: "0",
+               y: "3"
+             }
+  end
+
+  test "parse input with test fixture 3 (3)" do
+    assert Asciicanvas.parse_input(
+             "Rectangle at `[5, 5]` with width `5`, height `3`, outline character: `X`, fill: `X`"
+           ) ==
+             %{
+               fill: "X",
+               height: "3",
+               outline: "X",
+               type: :rectangle,
+               width: "5",
+               x: "5",
+               y: "5"
+             }
+  end
+
+  # test "parse input with test fixture 3 (4)" do
+  #   assert Asciicanvas.parse_input(
+  #            "Flood fill at `[0, 0]` with fill character `-` (canvas presented in 32x12 size)"
+  #          ) ==
+  #            %{
+  #              fill: "-",
+  #              height: nil,
+  #              outline: nil,
+  #              type: :flood,
+  #              width: nil,
+  #              x: "0",
+  #              y: "0"
+  #            }
+  # end
 end
