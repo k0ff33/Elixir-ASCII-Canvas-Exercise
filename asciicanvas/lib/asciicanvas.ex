165da2db7e55699dcc22551a354f7f7a4a62b544
Kamil Wojtczyk <k.wojtczyk@gmail.com>
2020-09-02 23:09:53 +0200

    feat: create initial string parsing function

--- asciicanvas/lib/asciicanvas.ex
+++ asciicanvas/lib/asciicanvas.ex
@@ -4,15 +4,32 @@ defmodule Asciicanvas do
   """
 
   @doc """
-  Hello world.
+  Parse ASCII canvas command into machine-readable format
 
   ## Examples
 
-      iex> Asciicanvas.hello()
-      :world
-
+      iex> Asciicanvas.parse_input("Rectangle at `[3,2]` with width: `5`, height: `3`, outline character: `@`, fill character: `X`")
+      %{
+        "fill" => "X",
+        "height" => "3",
+        "outline" => "@",
+        "type" => "Rectangle",
+        "width" => "5",
+        "x" => "3",
+        "y" => "2"
+      }
   """
-  def hello do
-    :world
+  def parse_input(input) do
+    regex =
+      ~r/(?<type>\w+)\[(?<x>.+),(?<y>.+)\].+width:(?<width>.+).+height:(?<height>.+).+outline:(?<outline>.+).+fill:(?<fill>.+)/
+
+    String.replace(input, " ", "")
+    |> String.replace("`", "")
+    |> String.replace("at", "")
+    |> String.replace("character", "")
+    |> (&Regex.named_captures(
+          regex,
+          &1
+        )).()
   end
 end
--- asciicanvas/test/asciicanvas_test.exs
+++ asciicanvas/test/asciicanvas_test.exs
@@ -1,8 +1,4 @@
 defmodule AsciicanvasTest do
   use ExUnit.Case
   doctest Asciicanvas
-
-  test "greets the world" do
-    assert Asciicanvas.hello() == :world
-  end
 end
