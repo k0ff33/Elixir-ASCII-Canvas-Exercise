d7ef9326cd155b2ba50ac53ff24372d56989bf52
Kamil Wojtczyk <k.wojtczyk@gmail.com>
2020-09-11 21:19:14 +0200

    fix: test environment config

--- canvas_server/config/test.exs
+++ canvas_server/config/test.exs
@@ -9,7 +9,7 @@ config :canvas_server, CanvasServer.Repo,
   username: "postgres",
   password: "postgres",
   database: "canvas_server_test#{System.get_env("MIX_TEST_PARTITION")}",
-  hostname: "localhost",
+  hostname: System.get_env("DB_HOST", "localhost"),
   pool: Ecto.Adapters.SQL.Sandbox
 
 # We don't run a server during test. If one is required,
